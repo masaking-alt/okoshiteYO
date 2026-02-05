9ebdf54dd43c5eaa6b78fd0f2c27989190052933
masaking-alt <banbenjianggui@gmail.com>
2026-02-06 06:27:35 +0900

    editorscreenをmainからコピペしてきたがエラー。

--- src/screens/EditorScreen.tsx
+++ src/screens/EditorScreen.tsx
@@ -67,9 +67,6 @@ const TimePicker: React.FC<TimePickerProps> = ({ value, onValueChange, type }) =
         maxLength={2}
         editable={true}
         selectTextOnFocus={true}
-        allowFontScaling={false}
-        multiline={false}
-        scrollEnabled={false}
       />
     </View>
   );
@@ -105,11 +102,9 @@ const EditorScreen: React.FC<Props> = ({
     // 空白または未入力の場合は「00」にする
     const finalHour = hour && hour !== '' ? twoDigit(parseInt(hour, 10)) : '00';
     const finalMinutes = minutes && minutes !== '' ? twoDigit(parseInt(minutes, 10)) : '00';
-    const title = memo.trim();
-    
+
     const payload: Alarm = {
       id: alarm?.id ?? Date.now().toString(),
-      title,
       time: `${finalHour}:${finalMinutes}`,
       repeatDays,
       action: selectedAction,
@@ -135,21 +130,19 @@ const EditorScreen: React.FC<Props> = ({
         <Text style={styles.toolbarTitle}>{alarm ? 'アラーム編集' : 'アラーム追加'}</Text>
         <View style={{ width: 24 }} />
       </View>
-
-      <Text style={styles.label}>時刻</Text>
-      <View style={styles.timeInputRow}>
-        <TimePicker value={hour} onValueChange={setHour} type="hour" />
-        <Text style={styles.timeColon}>:</Text>
-        <TimePicker value={minutes} onValueChange={setMinutes} type="minute" />
-      </View>
-
       <KeyboardAwareScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 48 }}
+        contentContainerStyle={{ paddingBottom: 100 }}
         enableOnAndroid={true}
         keyboardShouldPersistTaps="handled"
         extraScrollHeight={24}
       >
+        <Text style={styles.label}>時刻</Text>
+          <View style={styles.timeInputRow}>
+            <TimePicker value={hour} onValueChange={setHour} type="hour" />
+            <Text style={styles.timeColon}>:</Text>
+            <TimePicker value={minutes} onValueChange={setMinutes} type="minute" />
+          </View>
         <View style={styles.dayRow}>
           {days.map((day) => {
             const active = repeatDays.includes(day);
@@ -289,8 +282,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: palette.black,
     textAlign: 'center',
-    textAlignVertical: 'center',
-    includeFontPadding: false,
     width: 150
   },
   timeValueDisplay: {
